f00362b1b563fd5e82cbbd2d85645e9bf3b9dbe6
johnrip89 <lebronjohn89@gmail.com>
2021-11-19 12:25:50 -0700

    deleted reaction.js and added reaction schema to thought.js

--- models/Thought.js
+++ models/Thought.js
@@ -1,11 +1,38 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model, Types } = require('mongoose');
+
+const ReactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId()
+    },
+    reactionBody: {
+      type: String,
+      required: true
+    },
+    username: {
+      type: String,
+      required: true
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: createdAtVal => dateFormat(createdAtVal)
+    },
+  },
+  {
+    toJSON: {      
+      getters: true
+    }    
+  }
+);
 
 const ThoughtSchema = new Schema(
   {
     thoughtText: {
       type: String,
-      required: true,      
-    },    
+      required: true,
+    },
     createdAt: {
       type: Date,
       default: Date.now,
@@ -13,9 +40,9 @@ const ThoughtSchema = new Schema(
     },
     username: {
       type: String,
-      required: true      
+      required: true
     },
-    reactions: [reactionSchema]    
+    reactions: [ReactionSchema]
   },
   {
     toJSON: {
@@ -27,8 +54,10 @@ const ThoughtSchema = new Schema(
   }
 );
 
+
+
 ThoughtSchema.virtual('reactionCount').get(function () {
-  return this.reations.reduce    
+  return this.reations.reduce
 });
 
 const Thought = model('Thought', ThoughtSchema);
--- models/User.js
+++ models/User.js
@@ -17,8 +17,7 @@ const UserSchema = new Schema(
   },
   {
     toJSON: {
-      virtuals: true,
-      getters: true
+      virtuals: true      
     },
     id: false
   }
